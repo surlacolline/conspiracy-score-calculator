b6f4651456f7746cef9444d60f3b67f7a965242b
Elise Juif <EliseJuif@squarelab.ca>
2020-11-28 22:22:31 -0500

    style

--- src/app/pages/list-player-scores/list-player-scores.component.ts
+++ src/app/pages/list-player-scores/list-player-scores.component.ts
@@ -9,7 +9,7 @@ import { Score } from 'src/app/core/models/score.model';
   styleUrls: ['./list-player-scores.component.scss']
 })
 export class ListPlayerScoresComponent implements OnInit {
-  players: Player[] = [{ name: "Pierre", score: new Score() }, { name: "Charles", score: new Score() }];
+  players: Player[] = [{ name: "Elise", score: new Score() }, { name: "Hugo", score: new Score() }];
 
   constructor() { }
 
